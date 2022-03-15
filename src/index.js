09c3aa29ec6298cee7eab016690b5c27b5884eed
kinkade21 <victorpushanov@mail.ru>
2022-03-15 19:35:56 +0300

    dz27

--- src/index.js
+++ src/index.js
@@ -6,21 +6,47 @@ import calc from './modules/calc';
 import feedback from './modules/feedback';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import sendForm from './modules/sendForm';
 
-timer("30 march 2022 01:15");
+// таймер параметры: дата окончания акции
+timer("31 march 2022 01:15");
 menu();
 modal();
+// калькулятор параметры: стоимость одного квадратного метра
 calc(100);
 feedback();
 tabs();
+
+// слайдер параметры: css стили элементов, обязательный slider(<ul></ul>) slide(<li></li>)
 slider({
-    slider: 'portfolio-content',
-    slide: 'portfolio-item',
-    slideActive: 'portfolio-item-active',
-    button: 'portfolio-btn',
-    buttonPrev: 'prev',
-    buttonNext: 'next',
-    switchList: 'portfolio-dots',
-    switch: 'dot',
-    switchActive: 'dot-active',
+  slider: 'portfolio-content',
+  slide: 'portfolio-item',
+  slideActive: 'portfolio-item-active',
+  button: 'portfolio-btn',
+  buttonPrev: 'prev',
+  buttonNext: 'next',
+  switchList: 'portfolio-dots',
+  switch: 'dot',
+  switchActive: 'dot-active',
 }, 2000);
+
+// отправка формы параметры: сервер отправки, дополнительные данные к отправке
+sendForm('https://jsonplaceholder.typicode.com/posts',
+  {
+    // акция
+    form1: [{ name: "form", assign: "form1" },
+    { name: "total", select: "#total", assign: "textContent" }
+    ],
+    // вопросы
+    form2: [
+      { name: "form", assign: "form2" },
+      { name: "total", select: "#total", assign: "textContent" }
+    ],
+    // услуги
+    form3: [
+      { name: "form", assign: "form3" },
+      { name: "service", select: ".service-header-tab.active", assign: "textContent" },
+      { name: "total", select: "#total", assign: "textContent" }
+    ]
+  }
+);
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,10 +3,17 @@ import { animate } from './helpers';
 
 const menu = () => {
 
+    // панель заголовка
+    const header = document.querySelector('.main-header');
+
     // блок меню
     const menu = document.querySelector('menu');
+    // кнопка меню
+    const menuPush = document.querySelector('main header .menu');
+    const posStartFloatingMenu = document.querySelector('body .container')
+        .getBoundingClientRect().bottom + window.scrollY;
 
-    // плавный скролл по a.href  (пока, согласно задания, только ВНИЗ !!!)    
+    // плавный скролл по a.href   
     const smoothScroll = (e, href) => {
         e.preventDefault();
 
@@ -25,6 +32,19 @@ const menu = () => {
         });
     };
 
+    // изменение положения плавающего меню
+    const reFloatingMenu = () => {
+        if (window.scrollY > posStartFloatingMenu) {
+            menuPush.style.position = 'fixed';
+            menuPush.style.top = '10px';
+            menuPush.style.right = Math.max(10, window.innerWidth - header.offsetLeft - header.offsetWidth - 90) + 'px';
+            menuPush.style.zIndex = '1';
+            menuPush.style.opacity = '0.5';
+            menuPush.style.backgroundColor = 'var(--dark)';
+            menuPush.style.maxWidth = Math.round(80 / window.devicePixelRatio) + 'px';
+        } else if (menuPush.style) { menuPush.style = ""; }
+    };
+
     document.querySelector('body').addEventListener('click', (e) => {
         let itemMenu = null, nextBlock;
 
@@ -33,6 +53,7 @@ const menu = () => {
             (itemMenu = e.target.closest('menu ul>li>a')) ||    // по нажатию на пункт меню
             (!e.target.closest('.active-menu') &&           // по нажатии мимо окошка или крестик
                 menu.classList.contains('active-menu'))) {
+            e.preventDefault();
 
             // открытие/закрытие меню 
             menu.classList.toggle('active-menu');
@@ -45,6 +66,15 @@ const menu = () => {
 
         // плавный скролл по нажатию на пункт меню        
         if (itemMenu) { smoothScroll(e, itemMenu.getAttribute("href")); }
+    });
+
+
+    // плавающая кнопка меню
+    window.addEventListener('scroll', (e) => {
+        reFloatingMenu();
+    });
+    window.addEventListener('resize', (e) => {
+        reFloatingMenu();
     });
 
 }; 
