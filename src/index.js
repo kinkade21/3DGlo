e83452b3a4eee7ed646a45000d03ab8fc087ead7
kinkade21 <victorpushanov@mail.ru>
2022-03-15 18:40:30 +0300

    dz23

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import modal from './modules/modal';
 import calc from './modules/calc';
 import feedback from './modules/feedback';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer("30 march 2022 01:15");
 menu();
@@ -12,3 +13,14 @@ modal();
 calc();
 feedback();
 tabs();
+slider({
+    slider: 'portfolio-content',
+    slide: 'portfolio-item',
+    slideActive: 'portfolio-item-active',
+    button: 'portfolio-btn',
+    buttonPrev: 'prev',
+    buttonNext: 'next',
+    switchList: 'portfolio-dots',
+    switch: 'dot',
+    switchActive: 'dot-active',
+}, 2000);
